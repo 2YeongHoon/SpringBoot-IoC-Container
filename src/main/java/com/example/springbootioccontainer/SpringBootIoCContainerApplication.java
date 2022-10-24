07d628a3619d526f1d0face089035daefc7a399a
yeonghoon.lee <djaakclsrn12@naver.com>
2022-10-24 20:24:58 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootioccontainer/SpringBootIoCContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootioccontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIoCContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIoCContainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootioccontainer/SpringBootIoCContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootioccontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIoCContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
